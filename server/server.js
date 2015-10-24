b68116a6416a595ed90a59ee7c740bc15edab0fa
oteswang <oteswang@gmail.com>
2015-10-23 21:46:08 -1000

    fix spelling errors

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app     = express();
 
 app.get('/', function(req, res){
-  res.send('hellow world');
+  res.send('hello world');
 });
 
 app.listen(process.env.PORT || 8080, function(){
