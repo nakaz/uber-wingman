ba66601970d441e62c32cec7a31005b86ddf4894
Julia Bond <julia@yogaed.com>
2015-10-24 23:32:53 -1000

    Pseudo code added.

--- app/www/js/uberServices.js
+++ app/www/js/uberServices.js
@@ -1,5 +1,17 @@
 (function (){
   angular
     .module('wingman')
-    .service('uberServices');
+    .service('uberServices', ['$resource', 'BASE_URL',  function ($resource, BASE_URL){
+      this.requestUberData = function (latitude, longitude, radius){
+        var location = latitude + ',' + longitude;
+        var queryOptions = {
+          //insert query paramaters
+        };
+
+        //send to server api route
+        var UberData = $resource(BASE_URL + '/api/venues', queryOptions);
+        return UberData.query().$promise;
+      };
+
+    }]);
 })();
