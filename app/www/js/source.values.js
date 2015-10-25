bdbe6770c411dcfb8e795322f5ab8fd922dc9214
Sean Nakamura <seanenakamura@gmail.com>
2015-10-25 03:42:11 -1000

    added uber api urls

--- app/www/js/source.values.js
+++ app/www/js/source.values.js
@@ -3,5 +3,6 @@
   angular
     .module('wingman')
     .value('BASE_URL', 'http://' + LOCALHOST + ':8080')
-    .value('UBER_API', 'hello');
+    .value('UBER_API_ME', 'https://api.uber.com/v1/me')
+    .value('UBER_API_EP', 'https://api.uber.com/v1/estimates/price');
 })();
