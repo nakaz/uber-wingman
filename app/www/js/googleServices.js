dcd9734f60eed34171b78d9c74de6a56ba44c9db
Julia Bond <julia@yogaed.com>
2015-10-24 15:17:10 -1000

    Added google services.

--- app/www/js/googleServices.js
+++ app/www/js/googleServices.js
@@ -1,5 +1,19 @@
 (function (){
   angular
     .module('wingman')
-    .service('googleServices');
+    .service('googleServices', ['$resource', 'BASE_URL',  function ($resource, BASE_URL){
+      this.getVenues = function (latitude, longitude, radius){
+        var location = latitude + ',' + longitude;
+        var queryOptions = {
+          location: location,
+          radius: 16093.4, //equivalent of 10 miles
+          types: ['bar', 'night_club'],
+          key: process.env.GOOGLE_SERVER_TOKEN
+        };
+
+        var Venues = $resource(BASE_URL + '/api/venues', queryOptions);
+        return Venues.query().$promise;
+      };
+
+    }]);
 })();
