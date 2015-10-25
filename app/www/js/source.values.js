8128da3ec3b5037c700c6bed40744ba88608a8c6
Sean Nakamura <seanenakamura@gmail.com>
2015-10-25 04:08:35 -1000

    added uber api time estimates

--- app/www/js/source.values.js
+++ app/www/js/source.values.js
@@ -4,5 +4,6 @@
     .module('wingman')
     .value('BASE_URL', 'http://' + LOCALHOST + ':8080')
     .value('UBER_API_ME', 'https://api.uber.com/v1/me')
-    .value('UBER_API_EP', 'https://api.uber.com/v1/estimates/price');
+    .value('UBER_API_EP', 'https://api.uber.com/v1/estimates/price')
+    .value('UBER_API_TIME', 'https://api.uber.com/v1/estimates/time');
 })();
