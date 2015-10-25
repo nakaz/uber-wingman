c00d62e0b351721c31f8841a6b8d8f725d56e60a
Sean Nakamura <seanenakamura@gmail.com>
2015-10-25 05:56:54 -1000

    added uber ride request URL

--- app/www/js/source.values.js
+++ app/www/js/source.values.js
@@ -5,5 +5,6 @@
     .value('BASE_URL', 'http://' + LOCALHOST + ':8080')
     .value('UBER_API_ME', 'https://api.uber.com/v1/me')
     .value('UBER_API_EP', 'https://api.uber.com/v1/estimates/price')
-    .value('UBER_API_TIME', 'https://api.uber.com/v1/estimates/time');
+    .value('UBER_API_TIME', 'https://api.uber.com/v1/estimates/time')
+    .value('UBER_API_RIDE', 'https://sandbox-api.uber.com/v1/requests');
 })();
